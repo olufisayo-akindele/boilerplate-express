9ec400b4fbffe3798b6802d86af1eab8c2565400
olufisayo-akindele <67718162+olufisayo-akindele@users.noreply.github.com>
2023-01-28 18:14:13 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,29 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
 
+//require('dotenv').config() and you will need to (Create a .env file in the root of your project directory, and store the variable MESSAGE_STYLE=uppercase in it.)
 
-app.get('/', function(req, res) {
-  res.send('Hello Express');
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/json', function(req, res) {
+    const helloJSON = 'Hello json';
+    let message;
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        message = helloJSON.toUpperCase();
+    } else {
+        message = helloJSON;
+    }
+    const data = {
+        "message": message
+    };
+    res.json(data);
 })
 
+app.get('/', function(req, res) {
+    const filePath = __dirname + '/views/index.html';
+    console.log(filePath);
+    res.sendFile(filePath);
+});
 
 
 
@@ -37,5 +54,4 @@ app.get('/', function(req, res) {
 
 
 
-
- module.exports = app;
+module.exports = app;
