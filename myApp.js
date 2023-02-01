38450701b1c4371c75d91073902746e9d4cf918c
olufisayo-akindele <67718162+olufisayo-akindele@users.noreply.github.com>
2023-02-01 12:16:40 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,7 +3,24 @@ let app = express();
 
 //require('dotenv').config() and you will need to (Create a .env file in the root of your project directory, and store the variable MESSAGE_STYLE=uppercase in it.)
 
+
 app.use('/public', express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
+
+app.get('/:word/echo', function(req, res) {
+    res.json({ echo: req.params.word });
+});
+
+app.get('/now', function(req, res, next) {
+    req.time = (new Date()).toString();
+    next();
+}, function(req, res) {
+    res.json({ time: req.time });
+});
 
 app.get('/json', function(req, res) {
     const helloJSON = 'Hello json';
@@ -24,6 +41,10 @@ app.get('/', function(req, res) {
     console.log(filePath);
     res.sendFile(filePath);
 });
+
+
+
+
 
 
 
